1b0eb1b074ff463b59cad8bdd5e4779fdba0afa0
hugues <hsanterre1@gmail.com>
2023-03-28 16:38:09 -0400

    Améliorer l'interface, enlever les images inutiles, changer les pickle

--- 01_Modele.py
+++ 01_Modele.py
@@ -432,7 +432,6 @@ plt.bar([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22], df['Valeur_a
 plt.xticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22], df['Métrique'], rotation=70)
 plt.yticks([1,2,3,4])
 plt.gca().axes.get_yaxis().set_visible(False)
-plt.style.use('dark_background')
 
 st.pyplot(fig)
 
